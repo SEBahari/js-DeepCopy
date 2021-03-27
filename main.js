2de699f68046abd956858e309b71a9d9dbb4ab7b
ehsan <ebaharinasab.eb@gmail.com>
2021-03-27 15:01:33 +0430

    function improved with recursive call for object properties

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
-const obj1 = {};
-const obj2 = {};
+let obj1 = {
+    name: 'Ehsan Baharinasab',
+    id: 123456789,
+    courses: ['riazi1', 'fizik1'],
+    marks: {
+        riazi1: {
+            firstTerm: 12,
+            secondTerm: 13
+        },
+        fizik1: 12,
+    },
+};
+let obj2 = {};
 
 deepCopy(obj2, obj1);
 
-function deepCopy(target, Object) {
-    for (let key in Object) {
-        target[key] = Object[key];
+obj1.marks.riazi1 = 3;
+console.log(obj1, obj2);
+
+function deepCopy(target, object) {
+    for (let key in object) {
+        // console.log(key, object[key]);
+        if (typeof object[key] === "object" && !Array.isArray(object[key])) {
+            target[key] = {}
+            deepCopy(target[key], object[key]);
+        } else {
+            target[key] = object[key];
+        }
     }
 }
-
-console.log(obj2);
